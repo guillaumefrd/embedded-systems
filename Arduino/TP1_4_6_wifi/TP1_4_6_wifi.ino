3b54f172e657a641efe5ae7e70532f100006509f
Matthias Exbrayat <matt.exbrayat@hotmail.fr>
2018-01-26 09:57:29 +0100

    ajout de la question 3 de TP1_4_6_wifi

--- Arduino/TP1_4_6_wifi/TP1_4_6_wifi.ino
+++ Arduino/TP1_4_6_wifi/TP1_4_6_wifi.ino
@@ -6,27 +6,51 @@ void setup() {
   // put your setup code here, to run once:
   Bridge.begin();
   Serial.begin(9600);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   HttpClient client;
   client.get("http://www.ibo.net/");
-  char lettre_precedente = 'a';
+  char previous_letter = 'a';
   int count = 0;
   while(client.available()){
     char c = client.read();
-    if((c == 'o' || c == 'O') && (lettre_precedente == 'b' || lettre_precedente == 'B')){
+    if((c == 'o' || c == 'O') && (previous_letter == 'b' || previous_letter == 'B')){
       count++;
     }
-    lettre_precedente = c;
+    previous_letter = c;
     Serial.print(c);
   }
 
+  Serial.println(" ");
   Serial.print("Count = ");
   Serial.println(count);
   
   Serial.flush();
 
   delay(10000);
+
+  //Blink LED
+  while(true){
+    if(count < 25){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(5000);
+    }
+    else if(count < 50){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(2500);
+    }
+    else if(count < 100){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+    }
+    else{
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+    }
+  }
+  
 }
